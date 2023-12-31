036e9124c87a710e86a9aaccdc3bb9f23712091d
erfann31 <erfannasri2@gmail.com>
2023-12-31 03:42:15 +0330

    add OrderInformationWidget.dart

---
+++ lib/OrderInformationWidget.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+
+import 'models.dart';
+
+class OrderInformationWidget extends StatelessWidget {
+  final Order sampleOrder;
+
+  const OrderInformationWidget({required this.sampleOrder});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.only(bottom: 10),
+      color: const Color(0xffffffff),
+      shadowColor: const Color(0xff000000),
+      elevation: 1,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(12.0),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(8,10,8,10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Expanded(
+              flex: 1,
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(8, 0, 0, 0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    Text(
+                      "${sampleOrder.category}",
+                      textAlign: TextAlign.start,
+                      overflow: TextOverflow.clip,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.w700,
+                        fontStyle: FontStyle.normal,
+                        fontSize: 14,
+                        color: Color(0xff000000),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(0, 8, 0, 0),
+                      child: Text(
+                        "${sampleOrder.totalProducts} Products",
+                        textAlign: TextAlign.start,
+                        overflow: TextOverflow.clip,
+                        style: const TextStyle(
+                          fontWeight: FontWeight.w400,
+                          fontStyle: FontStyle.normal,
+                          fontSize: 12,
+                          color: Color(0xbe8a8989),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(0, 4, 0, 0),
+                      child: Text(
+                        "${sampleOrder.orderWeight.toString()} kg",
+                        textAlign: TextAlign.start,
+                        overflow: TextOverflow.clip,
+                        style: const TextStyle(
+                          fontWeight: FontWeight.w400,
+                          fontStyle: FontStyle.normal,
+                          fontSize: 12,
+                          color: Color(0xff8b8b8b),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Container(
+                  alignment: Alignment.center,
+                  margin: const EdgeInsets.fromLTRB(0, 8, 0, 0),
+                  padding: EdgeInsets.zero,
+                  width: MediaQuery.of(context).size.width * 0.20,
+                  height: MediaQuery.of(context).size.height * 0.06,
+                  decoration: BoxDecoration(
+                    color: const Color(0xff000000),
+                    shape: BoxShape.rectangle,
+                    borderRadius: BorderRadius.circular(22.0),
+                  ),
+                  child: const Text(
+                    "view",
+                    textAlign: TextAlign.start,
+                    overflow: TextOverflow.clip,
+                    style: TextStyle(
+                      fontWeight: FontWeight.w400,
+                      fontStyle: FontStyle.normal,
+                      fontSize: 16,
+                      color: Color(0xffffffff),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}

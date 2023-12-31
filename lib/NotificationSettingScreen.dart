612dbae35626d9192cdd0bfd0499d8d1e62c99e3
erfann31 <erfannasri2@gmail.com>
2023-12-31 04:54:12 +0330

    add onTap functions to NotificationSettingScreen.dart and Reformat

--- lib/NotificationSettingScreen.dart
+++ lib/NotificationSettingScreen.dart
@@ -2,160 +2,166 @@
 
 import 'package:flutter/material.dart';
 
-
 class NotificationSettingScreen extends StatelessWidget {
-
-@override
-Widget build(BuildContext context) {
-return Scaffold(
-backgroundColor: Color(0xffffffff),
-appBar: 
-AppBar(
-elevation:0,
-centerTitle:false,
-automaticallyImplyLeading: false,
-backgroundColor:Color(0xffffffff),
-shape:RoundedRectangleBorder(
-borderRadius:BorderRadius.zero,
-),
-title:Text(
-"Notification Setting",
-style:TextStyle(
-fontWeight:FontWeight.w700,
-fontStyle:FontStyle.normal,
-fontSize:20,
-color:Color(0xff000000),
-),
-),
-leading: Icon(
-Icons.arrow_back,
-color:Color(0xff212435),
-size:24,
-),
-),
-body:Padding(
-padding:EdgeInsets.all(16),
-child:
-Column(
-mainAxisAlignment:MainAxisAlignment.start,
-crossAxisAlignment:CrossAxisAlignment.start,
-mainAxisSize:MainAxisSize.max,
-children: [
-Row(
-mainAxisAlignment:MainAxisAlignment.spaceBetween,
-crossAxisAlignment:CrossAxisAlignment.center,
-mainAxisSize:MainAxisSize.max,
-children:[
-
-Text(
-"App Notification",
-textAlign: TextAlign.start,
-overflow:TextOverflow.clip,
-style:TextStyle(
-fontWeight:FontWeight.w400,
-fontStyle:FontStyle.normal,
-fontSize:16,
-color:Color(0xff000000),
-),
-),
-Checkbox(
-onChanged:(value) {},
-activeColor:Color(0xffff5630),
-autofocus:false,
-checkColor:Color(0xffffffff),
-hoverColor:Color(0x42000000),
-splashRadius:20,
-value:true,
-),
-],),
-Padding(
-padding:EdgeInsets.fromLTRB(0, 16, 0, 0),
-child:Row(
-mainAxisAlignment:MainAxisAlignment.spaceBetween,
-crossAxisAlignment:CrossAxisAlignment.center,
-mainAxisSize:MainAxisSize.max,
-children:[
-
-Text(
-"Recommended Article",
-textAlign: TextAlign.start,
-overflow:TextOverflow.clip,
-style:TextStyle(
-fontWeight:FontWeight.w400,
-fontStyle:FontStyle.normal,
-fontSize:16,
-color:Color(0xff000000),
-),
-),
-Checkbox(
-onChanged:(value) {},
-activeColor:Color(0xffff5630),
-autofocus:false,
-checkColor:Color(0xffffffff),
-hoverColor:Color(0x42000000),
-splashRadius:20,
-value:true,
-),
-],),),
-Padding(
-padding:EdgeInsets.fromLTRB(0, 16, 0, 0),
-child:Row(
-mainAxisAlignment:MainAxisAlignment.spaceBetween,
-crossAxisAlignment:CrossAxisAlignment.center,
-mainAxisSize:MainAxisSize.max,
-children:[
-
-Text(
-"Promotion",
-textAlign: TextAlign.start,
-overflow:TextOverflow.clip,
-style:TextStyle(
-fontWeight:FontWeight.w400,
-fontStyle:FontStyle.normal,
-fontSize:16,
-color:Color(0xff000000),
-),
-),
-Checkbox(
-onChanged:(value) {},
-activeColor:Color(0xffff5630),
-autofocus:false,
-checkColor:Color(0xffffffff),
-hoverColor:Color(0x42000000),
-splashRadius:20,
-value:true,
-),
-],),),
-Padding(
-padding:EdgeInsets.fromLTRB(0, 16, 0, 0),
-child:Row(
-mainAxisAlignment:MainAxisAlignment.spaceBetween,
-crossAxisAlignment:CrossAxisAlignment.center,
-mainAxisSize:MainAxisSize.max,
-children:[
-
-Text(
-"Latest News",
-textAlign: TextAlign.start,
-overflow:TextOverflow.clip,
-style:TextStyle(
-fontWeight:FontWeight.w400,
-fontStyle:FontStyle.normal,
-fontSize:16,
-color:Color(0xff000000),
-),
-),
-Checkbox(
-onChanged:(value) {},
-activeColor:Color(0xffff5630),
-autofocus:false,
-checkColor:Color(0xffffffff),
-hoverColor:Color(0x42000000),
-splashRadius:20,
-value:true,
-),
-],),),
-],),),
-)
-;}
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xffffffff),
+      appBar: AppBar(
+        elevation: 0,
+        centerTitle: false,
+        automaticallyImplyLeading: false,
+        backgroundColor: const Color(0xffffffff),
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.zero,
+        ),
+        title: const Text(
+          "Notification Setting",
+          style: TextStyle(
+            fontWeight: FontWeight.w700,
+            fontStyle: FontStyle.normal,
+            fontSize: 20,
+            color: Color(0xff252525),
+          ),
+        ),
+        leading: GestureDetector(
+          onTap: () {
+            Navigator.pop(context);
+          },
+          child: const Icon(
+            Icons.arrow_back,
+            color: Color(0xff252525),
+            size: 24,
+          ),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                const Text(
+                  "App Notification",
+                  textAlign: TextAlign.start,
+                  overflow: TextOverflow.clip,
+                  style: TextStyle(
+                    fontWeight: FontWeight.w400,
+                    fontStyle: FontStyle.normal,
+                    fontSize: 16,
+                    color: Color(0xff252525),
+                  ),
+                ),
+                Checkbox(
+                  onChanged: (value) {},
+                  activeColor: const Color(0xff252525),
+                  autofocus: false,
+                  checkColor: const Color(0xffffffff),
+                  hoverColor: const Color(0xff252525),
+                  splashRadius: 20,
+                  value: true,
+                ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 16, 0, 0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  const Text(
+                    "Recommended Article",
+                    textAlign: TextAlign.start,
+                    overflow: TextOverflow.clip,
+                    style: TextStyle(
+                      fontWeight: FontWeight.w400,
+                      fontStyle: FontStyle.normal,
+                      fontSize: 16,
+                      color: Color(0xff252525),
+                    ),
+                  ),
+                  Checkbox(
+                    onChanged: (value) {},
+                    activeColor: const Color(0xff252525),
+                    autofocus: false,
+                    checkColor: const Color(0xffffffff),
+                    hoverColor: const Color(0xff252525),
+                    splashRadius: 20,
+                    value: true,
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 16, 0, 0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  const Text(
+                    "Promotion",
+                    textAlign: TextAlign.start,
+                    overflow: TextOverflow.clip,
+                    style: TextStyle(
+                      fontWeight: FontWeight.w400,
+                      fontStyle: FontStyle.normal,
+                      fontSize: 16,
+                      color: Color(0xff252525),
+                    ),
+                  ),
+                  Checkbox(
+                    onChanged: (value) {},
+                    activeColor: const Color(0xff252525),
+                    autofocus: false,
+                    checkColor: const Color(0xffffffff),
+                    hoverColor: const Color(0xff252525),
+                    splashRadius: 20,
+                    value: true,
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 16, 0, 0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  const Text(
+                    "Latest News",
+                    textAlign: TextAlign.start,
+                    overflow: TextOverflow.clip,
+                    style: TextStyle(
+                      fontWeight: FontWeight.w400,
+                      fontStyle: FontStyle.normal,
+                      fontSize: 16,
+                      color: Color(0xff252525),
+                    ),
+                  ),
+                  Checkbox(
+                    onChanged: (value) {},
+                    activeColor: const Color(0xff252525),
+                    autofocus: false,
+                    checkColor: const Color(0xffffffff),
+                    hoverColor: const Color(0xff252525),
+                    splashRadius: 20,
+                    value: true,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }

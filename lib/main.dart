401d1e8d80bedf252c278aa67817136290e6b7fa
erfann31 <erfannasri2@gmail.com>
2023-12-31 04:27:47 +0330

    add navigations

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,46 @@
 import 'package:flutter/material.dart';
 
-import 'ContactUs.dart';
+import 'ContactUsScreen.dart';
 import 'LoginScreen.dart';
 import 'NoDataFoundScreen.dart';
-import 'detail.dart';
+import 'OrderDetailScreen.dart';
 import 'ChangePasswordScreen.dart';
+import 'consts.dart';
 import 'models.dart';
-import 'notifications.dart';
+import 'NotificationsScreen.dart';
 import 'NotificationSettingScreen.dart';
 import 'SettingScreen.dart';
+import 'HomeScreen.dart';
+import 'History.dart';
 
 void main() {
   runApp(MyApp());
 }
-
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'FlutterViz Demo',
-
-      /// TODO Replace with your first screen class name
-      home: SettingScreen(),
+      title: 'PickPall',
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/', // Set initial route
+      routes: {
+        '/': (context) => HomeScreen(), // Define your HomeScreen as the initial route
+        '/login': (context) => LoginScreen(), // Define LoginScreen route
+        '/orderDetail': (context) => OrderDetailScreen(sampleOrder: ordersList[0],), // Define OrderDetailScreen route
+        '/notifications': (context) => NotificationsScreen(), // Define NotificationsScreen route
+        '/settings': (context) => SettingScreen(), // Define SettingScreen route
+        '/contactUs': (context) => ContactUsScreen(), // Define ContactUsScreen route
+        '/changePassword': (context) => ChangePasswordScreen(), // Define ChangePasswordScreen route
+        '/notificationSettings': (context) => NotificationSettingScreen(), // Define NotificationSettingScreen route
+        '/history': (context) => HistoryScreen(), // Define HistoryScreen route
+        // Add more routes as needed
+        // '/routeName': (context) => YourScreen(), // Define YourScreen route
+        '/noDataFound': (context) => NoDataFoundScreen(), // Define NoDataFoundScreen route
+      },
+      onUnknownRoute: (settings) {
+        return MaterialPageRoute(builder: (context) => NoDataFoundScreen());
+      },
     );
   }
 }
+

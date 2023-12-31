08ff06ae8d0b257732591555d46499cabda3d13e
erfann31 <erfannasri2@gmail.com>
2023-12-31 04:28:01 +0330

    reformat LoginScreen.dart

--- lib/LoginScreen.dart
+++ lib/LoginScreen.dart
@@ -1,154 +1,149 @@
 ///File download from FlutterViz- Drag and drop a tools. For more details visit https://flutterviz.io/
 
 import 'package:flutter/material.dart';
-
+import 'package:pickpal/HomeScreen.dart';
 
 class LoginScreen extends StatelessWidget {
-
-@override
-Widget build(BuildContext context) {
-return Scaffold(
-backgroundColor: Color(0xffffffff),
-body:Align(
-alignment:Alignment.center,
-child:Padding(
-padding:EdgeInsets.fromLTRB(16, 50, 16, 16),
-child:SingleChildScrollView(
-child:
-Column(
-mainAxisAlignment:MainAxisAlignment.start,
-crossAxisAlignment:CrossAxisAlignment.start,
-mainAxisSize:MainAxisSize.max,
-children: [
-Text(
-"Login",
-textAlign: TextAlign.start,
-overflow:TextOverflow.clip,
-style:TextStyle(
-fontWeight:FontWeight.w700,
-fontStyle:FontStyle.normal,
-fontSize:24,
-color:Color(0xff000000),
-),
-),
-Padding(
-padding:EdgeInsets.fromLTRB(0, 30, 0, 16),
-child:TextField(
-controller:TextEditingController(),
-obscureText:false,
-textAlign:TextAlign.start,
-maxLines:1,
-style:TextStyle(
-fontWeight:FontWeight.w400,
-fontStyle:FontStyle.normal,
-fontSize:14,
-color:Color(0xff000000),
-),
-decoration:InputDecoration(
-disabledBorder:OutlineInputBorder(
-borderRadius:BorderRadius.circular(12.0),
-borderSide:BorderSide(
-color:Color(0x00ffffff),
-width:1
-),
-),
-focusedBorder:OutlineInputBorder(
-borderRadius:BorderRadius.circular(12.0),
-borderSide:BorderSide(
-color:Color(0x00ffffff),
-width:1
-),
-),
-enabledBorder:OutlineInputBorder(
-borderRadius:BorderRadius.circular(12.0),
-borderSide:BorderSide(
-color:Color(0x00ffffff),
-width:1
-),
-),
-hintText:"Email",
-hintStyle:TextStyle(
-fontWeight:FontWeight.w400,
-fontStyle:FontStyle.normal,
-fontSize:14,
-color:Color(0xff9f9d9d),
-),
-filled:true,
-fillColor:Color(0xfff2f2f3),
-isDense:false,
-contentPadding:EdgeInsets.symmetric(vertical: 8,horizontal:12),
-),
-),
-),
-Padding(
-padding:EdgeInsets.fromLTRB(0, 0, 0, 16),
-child:TextField(
-controller:TextEditingController(),
-obscureText:false,
-textAlign:TextAlign.start,
-maxLines:1,
-style:TextStyle(
-fontWeight:FontWeight.w400,
-fontStyle:FontStyle.normal,
-fontSize:14,
-color:Color(0xff000000),
-),
-decoration:InputDecoration(
-disabledBorder:OutlineInputBorder(
-borderRadius:BorderRadius.circular(12.0),
-borderSide:BorderSide(
-color:Color(0x00ffffff),
-width:1
-),
-),
-focusedBorder:OutlineInputBorder(
-borderRadius:BorderRadius.circular(12.0),
-borderSide:BorderSide(
-color:Color(0x00ffffff),
-width:1
-),
-),
-enabledBorder:OutlineInputBorder(
-borderRadius:BorderRadius.circular(12.0),
-borderSide:BorderSide(
-color:Color(0x00ffffff),
-width:1
-),
-),
-hintText:"Password",
-hintStyle:TextStyle(
-fontWeight:FontWeight.w400,
-fontStyle:FontStyle.normal,
-fontSize:14,
-color:Color(0xff9f9d9d),
-),
-filled:true,
-fillColor:Color(0xfff2f2f3),
-isDense:false,
-contentPadding:EdgeInsets.symmetric(vertical: 8,horizontal:12),
-),
-),
-),
-Padding(
-padding:EdgeInsets.fromLTRB(0, 30, 0, 16),
-child:MaterialButton(
-onPressed:(){},
-color:Color(0xffff5630),
-elevation:0,
-shape:RoundedRectangleBorder(
-borderRadius:BorderRadius.circular(12.0),
-),
-padding:EdgeInsets.all(16),
-child:Text("Button", style: TextStyle( fontSize:16,
-fontWeight:FontWeight.w700,
-fontStyle:FontStyle.normal,
-),),
-textColor:Color(0xffffffff),
-height:40,
-minWidth:MediaQuery.of(context).size.width,
-),
-),
-],),),),),
-)
-;}
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xffffffff),
+      body: Align(
+        alignment: Alignment.center,
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(16, 50, 16, 16),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                const Text(
+                  "Login",
+                  textAlign: TextAlign.start,
+                  overflow: TextOverflow.clip,
+                  style: TextStyle(
+                    fontWeight: FontWeight.w700,
+                    fontStyle: FontStyle.normal,
+                    fontSize: 24,
+                    color: Color(0xff000000),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 30, 0, 16),
+                  child: TextField(
+                    controller: TextEditingController(),
+                    obscureText: false,
+                    textAlign: TextAlign.start,
+                    maxLines: 1,
+                    style: const TextStyle(
+                      fontWeight: FontWeight.w400,
+                      fontStyle: FontStyle.normal,
+                      fontSize: 14,
+                      color: Color(0xff000000),
+                    ),
+                    decoration: InputDecoration(
+                      disabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12.0),
+                        borderSide: const BorderSide(color: Color(0x00ffffff), width: 1),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12.0),
+                        borderSide: const BorderSide(color: Color(0x00ffffff), width: 1),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12.0),
+                        borderSide: const BorderSide(color: Color(0x00ffffff), width: 1),
+                      ),
+                      hintText: "Email",
+                      hintStyle: const TextStyle(
+                        fontWeight: FontWeight.w400,
+                        fontStyle: FontStyle.normal,
+                        fontSize: 14,
+                        color: Color(0xff9f9d9d),
+                      ),
+                      filled: true,
+                      fillColor: const Color(0xfff2f2f3),
+                      isDense: false,
+                      contentPadding: const EdgeInsets.symmetric(vertical: 8, horizontal: 12),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 0, 0, 16),
+                  child: TextField(
+                    controller: TextEditingController(),
+                    obscureText: false,
+                    textAlign: TextAlign.start,
+                    maxLines: 1,
+                    style: const TextStyle(
+                      fontWeight: FontWeight.w400,
+                      fontStyle: FontStyle.normal,
+                      fontSize: 14,
+                      color: Color(0xff000000),
+                    ),
+                    decoration: InputDecoration(
+                      disabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12.0),
+                        borderSide: const BorderSide(color: Color(0x00ffffff), width: 1),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12.0),
+                        borderSide: const BorderSide(color: Color(0x00ffffff), width: 1),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12.0),
+                        borderSide: const BorderSide(color: Color(0x00ffffff), width: 1),
+                      ),
+                      hintText: "Password",
+                      hintStyle: const TextStyle(
+                        fontWeight: FontWeight.w400,
+                        fontStyle: FontStyle.normal,
+                        fontSize: 14,
+                        color: Color(0xff9f9d9d),
+                      ),
+                      filled: true,
+                      fillColor: const Color(0xfff2f2f3),
+                      isDense: false,
+                      contentPadding: const EdgeInsets.symmetric(vertical: 8, horizontal: 12),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 30, 0, 16),
+                  child: MaterialButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => HomeScreen(),
+                        ),
+                      );
+                    },
+                    color: const Color(0xffff5630),
+                    elevation: 0,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12.0),
+                    ),
+                    padding: const EdgeInsets.all(16),
+                    textColor: const Color(0xffffffff),
+                    height: 40,
+                    minWidth: MediaQuery.of(context).size.width,
+                    child: const Text(
+                      "Login",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w700,
+                        fontStyle: FontStyle.normal,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 }
